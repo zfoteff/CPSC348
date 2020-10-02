9481c94e97be9278a50a31515d12e4f7ae7243ce
zfoteff <zfoteff@zagmail.gonzaga.edu>
2020-10-01 21:22:18 -0700

    Finished tests

--- hw1/rsa_cracker.py
+++ hw1/rsa_cracker.py
@@ -11,7 +11,6 @@ File Name: rsa_cracker.py
 
 from math import sqrt, floor
 import time
-import mpmath
 from sympy.ntheory import factorint
 
 
@@ -24,7 +23,10 @@ def inverse_mod(a, m):
         return x % m
 
 def extended_greatest_common_denominator(a, b):
-    """Return (g, x, y) such that a*x + b*y = g = greatest_common_denominator(a, b)."""
+    """
+    Return (g, x, y) such that
+        a*x + b*y = g = greatest_common_denominator(a, b)
+    """
     if a == 0:
         return (b, 0, 1)
     else:
@@ -32,10 +34,28 @@ def extended_greatest_common_denominator(a, b):
         return (g, x - (b // a) * y, y)
 
 def encryptRSA(e, n , text):
+    """
+    Encrypt a plaintext message to ciphertext using RSA encyption algorithm
+
+    Keyword Arguments:
+    e -- public exponent
+    n -- modulus
+    text -- plaintext
+    """
+
     cyphertext = pow(text, e, n)
     return cyphertext
 
 def decryptRSA(d, n, text):
+    """
+    Decrypt a ciphertext message to plaintext using RSA decryption algorithm
+
+    Keyword Arguments:
+    d -- private exponent
+    n -- modulus
+    text -- ciphertext
+    """
+
     plaintext = pow(text, d, n)
     return plaintext
 
@@ -69,6 +89,7 @@ def crackRsaBruteForce (e, n):
     """
     p = getFirstFactor(n)
     q = n/p
+    # phi = Euler Tortient
     phi = (p-1)*(q-1)
 
     d = 1
@@ -86,8 +107,9 @@ def crackRsaPrivateKey(e, n):
     """Given an RSA private key, return the private exponent."""
     factorsOfN = factorint(n)
 
-    # since n is the product of two primes, we know its only factors are those two primes
-    # if this is not the case, n is invalid (i.e., not the product of two primes)
+    # since n is the product of two primes, we know its only factors are those
+    # two primes if this is not the case, n is invalid (i.e., not the product of
+    # two primes)
     factorsAreValid = len(factorsOfN) == 2 and all(multiplicity == 1 for multiplicity in factorsOfN.values())
     if (not factorsAreValid):
         raise Exception(f"{n} is not a valid n value.")
@@ -100,19 +122,10 @@ def crackRsaPrivateKey(e, n):
     d = inverse_mod(e, phi)
     return d
 
-def testAlgorithms ():
-    key_sizes = [6,6,7,8,10,11,25,30,33,55,68,73,81]
-    e_list =    [7,3,17,11,7,1319,20293957,293210035,5292724727,65537,65537,65537,65537]
-    n_list =    [33,55,77,143,527,1717,24899297,660416987,8093802337,26205577898256853,221000191580019839159,23800004200970185016117,1769685722799684125246893]
-    d_list =    [3,27,53,11,343,0,17590093,0,0,9843735732800705,0,0,0]
-    plaintext = [11,22,33,44,55,66,77,88,99,1010,1111,1212,1313]
-    encrypted = [11,33,66,99,251,263,3851853,159810131,6516777357,9190603619039538,10883406131699884500660416987,7,10290404458130915244793,1596396698114749970315290]
-
-    # Repeat the test cases 13 times
-    for case in range (0, 13):
-        printAlgorithmResults (case, key_sizes[case], plaintext[case], encrypted[case], e_list[case], n_list[case], d_list[case])
-
 def printAlgorithmResults (test_case, key_size, plaintext, ciphertext, e, n, d):
+    """
+    Test and output the results of
+    """
     brute_force_time = -1
 
     if test_case < 7:
@@ -136,13 +149,26 @@ def printAlgorithmResults (test_case, key_size, plaintext, ciphertext, e, n, d):
     print ("cracking took "+str(cracking_time)+" seconds")
 
     if brute_force_time == -1:
-        print ("brute force took too long to complete\n")
+        print ("could not crack the key using brute force\n")
     else:
         print ("brute force took "+str(brute_force_time)+" seconds\n")
 
 def main():
-    """ Main function """
-    testAlgorithms()
+    key_sizes = [6,6,7,8,10,11,25,30,33,55,68,73,81]
+    e_list =    [7,3,17,11,7,1319,20293957,293210035,5292724727,65537,65537,
+                65537,65537]
+    n_list =    [33,55,77,143,527,1717,24899297,660416987,8093802337,
+                26205577898256853,221000191580019839159,23800004200970185016117,
+                1769685722799684125246893]
+    d_list =    [3,27,53,11,343,0,17590093,0,0,9843735732800705,0,0,0]
+    plaintext = [11,22,33,44,55,66,77,88,99,1010,1111,1212,1313]
+    encrypted = [11,33,66,99,251,263,3851853,159810131,6516777357,
+                9190603619039538,108834061316998845007,10290404458130915244793,
+                1596396698114749970315290]
+
+    # Repeat the for all test cases
+    for case in range (0, len(key_sizes)):
+        printAlgorithmResults (case, key_sizes[case], plaintext[case], encrypted[case], e_list[case], n_list[case], d_list[case])
 
 
 main()
